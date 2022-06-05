b5edee099c91614d8a2ba1b383a1a901a90c86a8
Nicolás Cartier <ncartier@navent.com>
2022-06-04 23:43:35 -0300

    Agrego fecha de creacion y publisher

--- collections/integrador/src/main/java/model/Posting.java
+++ collections/integrador/src/main/java/model/Posting.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Date;
+
 public class Posting {
 
     private Integer postingId;
@@ -8,6 +10,9 @@ public class Posting {
     private RealestateTypeEnum realestateType;
     private OperationTypeEnum operationType;
     private PostingType postingType;
+    private Publisher publisher;
     private Double quality;
     private StatusEnum status;
+    private Date creationDate;
+    private Date updateDate;
 }
